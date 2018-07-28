efb064faf3629581ccc955d99144ca3db8e6ec8f
swaleha19 <sheikhswaleha17@gmail.com>
2018-07-28 16:25:01 +0530

    restraunt

--- layout/home.php
+++ layout/home.php
@@ -147,7 +147,24 @@ input[type=number]::-webkit-outer-spin-button {
 
 		</div>
 	</nav>
+<script>
+function msg(){
+	alert("A");
+	var e=/^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
+	var p=document.getElementById("u_pass").value;
+	if(!e.test(p)){
+	document.getElementById("pp").innerHTML="password must be 1 upper and 1 Lower case and 8 char";
+	}
+	else {
 
+	document.getElementById("pp").innerHTML="valid";
+	}
+
+}
+
+
+
+</script>
 
 		<div id='login' class='modal fade' role='dialog'>
 		<div class='modal-dialog'>
@@ -168,12 +185,14 @@ input[type=number]::-webkit-outer-spin-button {
 				</div><br>
 				<div class="row">
 					<input type="text" id='u_login' class='form-control' name="uname" placeholder="Username or Email">
+					<p id="loginmsg"></p>
 				</div><br>
 				<div class="row">
 					Password
 				</div><br>
 				<div class="row">
-					<input type="password" id="u_pass" class='form-control' name="pass" placeholder="Password">
+					<input type="password" id="u_pass" class='form-control' name="pass"  onblur="msg()" placeholder="Password">
+								<div id="pp">aa</div>
 				</div><br>
 				<div class="row">
 					<div class="col-lg-2 col-sm-2">
@@ -327,7 +346,7 @@ input[type=number]::-webkit-outer-spin-button {
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
 					<h2 class="cursive-font primary-color">Popular Dishes</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+					<p></p>
 				</div>
 			</div>
 			<div class="row"><?php
@@ -386,7 +405,7 @@ input[type=number]::-webkit-outer-spin-button {
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
 					<h2 class="cursive-font">Our Services</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+					<p></p>
 				</div>
 			</div>
 			<div class="row">
@@ -396,7 +415,7 @@ input[type=number]::-webkit-outer-spin-button {
 							<i class="ti-face-smile"></i>
 						</span>
 						<h3>Happy People</h3>
-						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						<p></p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
@@ -405,7 +424,7 @@ input[type=number]::-webkit-outer-spin-button {
 							<i class="ti-thought"></i>
 						</span>
 						<h3>Creative Culinary</h3>
-						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						<p></p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
@@ -414,7 +433,7 @@ input[type=number]::-webkit-outer-spin-button {
 							<i class="ti-truck"></i>
 						</span>
 						<h3>Food Delivery</h3>
-						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						<p></p>
 					</div>
 				</div>
 
@@ -424,88 +443,14 @@ input[type=number]::-webkit-outer-spin-button {
 	</div>
 
 
-	<div class="gtco-cover gtco-cover-sm" style="background-image: url(../images/img_bg_1.jpg)"  data-stellar-background-ratio="0.5">
-		<div class="overlay"></div>
-		<div class="gtco-container text-center">
-			<div class="display-t">
-				<div class="display-tc">
-					<h1>&ldquo; Their high quality of service makes me back over and over again!&rdquo;</h1>
-					<p>&mdash; John Doe, CEO of XYZ Co.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div id="gtco-counter" class="gtco-section">
-		<div class="gtco-container">
-
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
-					<h2 class="cursive-font primary-color">Fun Facts</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
-			</div>
-
-			<div class="row">
-
-				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-					<div class="feature-center">
-						<span class="counter js-counter" data-from="0" data-to="5" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Avg. Rating</span>
-
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-					<div class="feature-center">
-						<span class="counter js-counter" data-from="0" data-to="43" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Food Types</span>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-					<div class="feature-center">
-						<span class="counter js-counter" data-from="0" data-to="32" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Chef Cook</span>
-					</div>
-				</div>
-				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
-					<div class="feature-center">
-						<span class="counter js-counter" data-from="0" data-to="1985" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Year Started</span>
-
-					</div>
-				</div>
-
-			</div>
-		</div>
-	</div>
 
 
 
-	<div id="gtco-subscribe">
-		<div class="gtco-container">
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
-					<h2 class="cursive-font">Subscribe</h2>
-					<p>Be the first to know about the new templates.</p>
-				</div>
-			</div>
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2">
-					<form class="form-inline">
-						<div class="col-md-6 col-sm-6">
-							<div class="form-group">
-								<label for="email" class="sr-only">Email</label>
-								<input type="email" class="form-control" id="email" placeholder="Your Email">
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-6">
-							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+
+
+
+
+
 
 	<footer id="gtco-footer" role="contentinfo" style="background-image: url(images/img_bg_1.jpg)" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
@@ -519,7 +464,7 @@ input[type=number]::-webkit-outer-spin-button {
 					<div class="gtco-widget">
 						<h3>Get In Touch</h3>
 						<ul class="gtco-quick-contact">
-							<li><a href="#"><i class="icon-phone"></i> +1 234 567 890</a></li>
+							<li><a href="#"><i class="icon-phone"></i>7587428786</a></li>
 							<li><a href="#"><i class="icon-mail2"></i> info@GetTemplates.co</a></li>
 							<li><a href="#"><i class="icon-chat"></i> Live Chat</a></li>
 						</ul>
